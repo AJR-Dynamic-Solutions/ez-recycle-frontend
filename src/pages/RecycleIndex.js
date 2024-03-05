cb0b1ca67c533a218899d79728a3764fd0d4d8eb
RoderickVongsa <roderick.vongsa@gmail.com>
2024-03-05 13:58:42 -0800

    finished recycle show, fixed fragment on index.js

--- src/pages/RecycleIndex.js
+++ src/pages/RecycleIndex.js
@@ -11,42 +11,31 @@ import { NavLink } from "react-router-dom";
 
 const RecycleIndex = ({ recycles }) => {
   return (
-    <>
-      <div className="recycles-body">
-        <h1>Recent Listings</h1>
-        <div className="flex-recycles">
-          {recycles?.map((recycle, index) => {
-            return (
-              <Card key={index} className="recycle-cards">
-                <CardImg
-                  top
-                  width="100%"
-                  src={recycle.image}
-                  alt=""
-                  className="recycle-picture"
-                />
-                <CardBody>
-                  <div className="recycle-text">
-                    <CardTitle>
-                      <b>${recycle.price}</b>
-                    </CardTitle>
-                    <CardSubtitle>
-                      {recycle.item}, {recycle.city}, {recycle.state}
-                    </CardSubtitle>
-                  </div>
-                  <NavLink
-                    to={`/recycleshow/${recycle.id}`}
-                    className="nav-link"
-                  >
-                    <Button className="recycle-button">More Details</Button>
-                  </NavLink>
-                </CardBody>
-              </Card>
-            );
-          })}
-        </div>
+    <div className="recycles-body">
+      <h1>Recent Listings</h1>
+      <div className="flex-recycles">
+        {recycles?.map((recycle, index) => {
+          return (
+            <Card key={index} className="recycle-cards">
+              <CardImg src={recycle.image} alt="" className="recycle-picture" />
+              <CardBody>
+                <div className="recycle-text">
+                  <CardTitle>
+                    <b>${recycle.price}</b>
+                  </CardTitle>
+                  <CardSubtitle>
+                    {recycle.item}, {recycle.city}, {recycle.state}
+                  </CardSubtitle>
+                </div>
+                <NavLink to={`/recycleshow/${recycle.id}`} className="nav-link">
+                  <Button className="recycle-button">More Details</Button>
+                </NavLink>
+              </CardBody>
+            </Card>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 };
 export default RecycleIndex;
