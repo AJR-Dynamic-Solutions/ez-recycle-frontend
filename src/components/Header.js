e61a34f5c279b32884434a298116c928b055d4f2
AndrewBalink <andrew.balink@gmail.com>
2024-03-07 13:26:02 -0800

    completed signup and signin functionality and testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 import { Nav, NavItem } from "reactstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 
-const Header = (currentUser) => {
-  const navigate = useNavigate()
+const Header = ({ currentUser, logout }) => {
+  const navigate = useNavigate();
   const handleClick = () => {
-    navigate("/")
-  }
+    logout();
+    navigate("/");
+  };
+  console.log(currentUser);
   return (
- <>
+    <>
       <Nav>
         <NavItem>
           <NavLink to="/" className="nav-link">
@@ -45,7 +47,7 @@ const Header = (currentUser) => {
         {!currentUser && (
           <>
             <NavItem>
-              <NavLink to="/login" className="nav-link">
+              <NavLink to="/signin" className="nav-link">
                 Sign In
               </NavLink>
             </NavItem>
@@ -58,7 +60,7 @@ const Header = (currentUser) => {
         )}
       </Nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
